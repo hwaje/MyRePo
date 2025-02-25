35ccc60ec4fd41ccf1fe521e42808216f28cebd6
hyunsoft <77khnam@gmail.com>
2025-02-25 21:23:44 +0900

    upload python file

---
+++ day2/ArithmeticOperations.py
@@ -0,0 +1,29 @@
+# 사칙연산 함수 정의
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero is not allowed"
+
+
+# 테스트 코드
+if __name__ == '__main__':
+    a = 5
+    b = 10
+
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {subtract(a, b)}")
+    print(f"{a} * {b} = {multiply(a, b)}")
+    print(f"{a} / {b} = {divide(a, b)}")
